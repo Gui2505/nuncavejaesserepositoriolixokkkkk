5aed464137a786f502501676aa991b9010a73e1e
Sonmethings <95493070+Gui2505@users.noreply.github.com>
2025-02-04 10:43:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+var htmlContent = `
+<!DOCTYPE html>
+<title>Hacked By Sonmethings </title>
+<link rel="icon" href="https://i.pinimg.com/564x/c6/3b/eb/c63bebbac975233420c715666e818453.jpg" type="image/x-icon"/>
+<link rel="shortcut icon" href="https://i.pinimg.com/564x/c6/3b/eb/c63bebbac975233420c715666e818453.jpg" type="image/x-icon"/>
+<meta charset="UTF-8">
+<body bgcolor="black">
+  <style type="text/css">/* latin */ @font-face { font-family: 'Iceberg'; font-style: normal; font-weight: 400; src: local('Iceberg'), local('Iceberg-Regular'), url(https://fonts.gstatic.com/s/iceberg/v7/8QIJdijAiM7o-qnZiI8EqprnEO0.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style> 
+  <style type="text/css">/* latin */ @font-face { font-family: 'Wallpoet'; font-style: normal; font-weight: 400; src: local('Wallpoet'), url(https://fonts.gstatic.com/s/wallpoet/v11/f0X10em2_8RnXVVdUObp58Tt868H.woff2) format('woff2'); unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD; }</style>  
+  <style>
+    h1{
+      align-content: center;
+      font-size: 46px;
+    }
+    h4{
+      align-content: center;
+      font-size: 20px;
+    }
+    h3{
+      align-content: center;
+    }
+  </style>
+  
+  <center>
+    <font face="Iceberg" size="6" style="text-shadow: 0px 0px 20px green;" color="green">
+      <b>OWNED BY</b>
+    </font>
+    <font face="Iceberg" size="6" style="text-shadow: 0px 0px 20px red;" color="red">
+      <b>@GT9SEC</b>
+    </font>
+    <hr>
+    <iframe src="https://emupedia.net/emupedia-game-doom1/" width="1000" height="600" frameborder="0" allowfullscreen></iframe>
+    <hr>
+    <marquee style="font-size: 30px; font-weight: bold; color: green; font-family: 'Iceberg'">Instagram: @gt9sec Youtube: www.youtube.com/@gt9sec Github: github.com/gt9sec Telegram: t.me/gt9sec</marquee><p><hr>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/3S2DmZI9xZ8?si=ot9kQIY7jihaCo3z" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>  
+  
+  
+  
+  </center>
+</body>
+</html>
+`;
+
+document.body.innerHTML = htmlContent;
